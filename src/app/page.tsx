8c11c2edfd60a497e9138d6a1c3c8e95f85d0df9
Ommer1453 Was Here! <omer.cetin@logo.com.tr>
2025-06-03 18:39:06 +0300

    ks-6 mainpage1453

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+       
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -39,7 +40,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Deploy now 1453
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
